8bbb33b7407df56f8fe67759a53706d8c22919c1
David Araujo <davidsdearaujo@gmail.com>
2020-06-23 00:38:56 -0300

    Adicionados scripts para rodar o exemplo em modo home ou em modo rotas

--- example/lib/main_home.dart
+++ example/lib/main_home.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
 
-void main() async {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
